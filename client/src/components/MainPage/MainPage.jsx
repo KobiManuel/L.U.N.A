a8c272dd2e77f2480dda90418ae3d7748a053b20
KobiManuel <emmakobi91@gmail.com>
2023-10-08 15:03:05 +0100

    feat: added limiters to name input

--- client/src/components/MainPage/MainPage.jsx
+++ client/src/components/MainPage/MainPage.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import styles from "./MainPage.module.scss";
-import robot from "../../assets/intror.jpg";
 import Header from "../Header/Header";
 
 const MainPage = () => {
+  const handleTextInput = (e) => {
+    const value = e.target.value.trim();
+    const words = value.split(/\s+/);
+    if (words.length === 2) {
+      e.target.value = words.slice(0, 2).join(" ");
+    }
+  };
+
   return (
     <main className={styles.main}>
       <Header />
@@ -21,7 +28,11 @@ const MainPage = () => {
           An A.I chat application created with Vite, React.js and Sass.
           Utilizing the Open AI API to send prompts and receive responses
         </p>
-        <input placeholder="e.g. Elon Musk" className={styles["name-input"]} />
+        <input
+          placeholder="e.g. Elon Musk"
+          className={styles["name-input"]}
+          onChange={handleTextInput}
+        />
         <button className={styles["get-started-btn"]}>Get Started</button>
       </div>
     </main>
