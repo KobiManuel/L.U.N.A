2496d7eab58d32036fc043d9fbdad3fc973e2997
KobiManuel <emmakobi91@gmail.com>
2023-10-09 08:43:52 +0100

    feat: added validation and error

--- client/src/components/MainPage/MainPage.jsx
+++ client/src/components/MainPage/MainPage.jsx
@@ -5,6 +5,7 @@ import Header from "../Header/Header";
 
 const MainPage = () => {
   const [isHovered, setIsHovered] = useState(false);
+  const [error, setError] = useState(false);
 
   const inputRef = useRef(null);
 
@@ -13,6 +14,9 @@ const MainPage = () => {
   const handleTextInput = (e) => {
     const value = e.target.value.trim();
     const words = value.split(/\s+/);
+    if (error) {
+      setError(false);
+    }
     if (words.length === 2) {
       e.target.value = words.slice(0, 2).join(" ");
     }
@@ -25,8 +29,18 @@ const MainPage = () => {
   };
 
   const handleGetStarted = () => {
-    localStorage.setItem("lunaClient", inputRef?.current?.value);
-    navigate("chat");
+    const value = inputRef.current.value.trim();
+    const words = value.split(/\s+/);
+    const [first, last] = words;
+
+    if (!first || !last) {
+      setError(true);
+    }
+
+    if (first && last) {
+      localStorage.setItem("lunaClient", value);
+      navigate("/chat");
+    }
   };
 
   return (
@@ -46,12 +60,21 @@ const MainPage = () => {
           An A.I chat application created with Vite, React.js and Sass.
           Utilizing the Open AI API to send prompts and receive responses
         </p>
-        <input
-          placeholder="e.g. Elon Musk"
-          className={styles["name-input"]}
-          onChange={handleTextInput}
-          ref={inputRef}
-        />
+        <div style={{ position: "relative", width: "fit-content" }}>
+          <input
+            placeholder="e.g. Elon Musk"
+            className={styles["name-input"]}
+            onChange={handleTextInput}
+            ref={inputRef}
+          />
+          {error ? (
+            <span className={styles["error-msg"]}>
+              Input should contain two names
+            </span>
+          ) : (
+            ""
+          )}
+        </div>
         <div
           style={{ width: "fit-content" }}
           onMouseOver={handleMouseOver}
