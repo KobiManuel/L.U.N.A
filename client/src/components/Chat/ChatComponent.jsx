501b803870ff38ffca0a96072c1168f7dd9785c8
KobiManuel <emmakobi91@gmail.com>
2023-10-18 13:38:36 +0100

    feat: screen record test phase

--- client/src/components/Chat/ChatComponent.jsx
+++ client/src/components/Chat/ChatComponent.jsx
@@ -95,55 +95,6 @@ const ChatComponent = () => {
 
     const uniqueId = generateUniqueId();
 
-    const timeoutMessage =
-      "This might take a couple more seconds, server is spinning up.";
-
-    const timeoutMessageWithSVG = (
-      <div>
-        {timeoutMessage}
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 14 14"
-        >
-          <circle cx="18" cy="12" r="0" fill="currentColor">
-            <animate
-              attributeName="r"
-              begin=".67"
-              calcMode="spline"
-              dur="1.5s"
-              keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
-              repeatCount="indefinite"
-              values="0;2;0;0"
-            />
-          </circle>
-          <circle cx="12" cy="12" r="0" fill="currentColor">
-            <animate
-              attributeName="r"
-              begin=".33"
-              calcMode="spline"
-              dur="1.5s"
-              keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
-              repeatCount="indefinite"
-              values="0;2;0;0"
-            />
-          </circle>
-          <circle cx="6" cy="12" r="0" fill="currentColor">
-            <animate
-              attributeName="r"
-              begin="0"
-              calcMode="spline"
-              dur="1.5s"
-              keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
-              repeatCount="indefinite"
-              values="0;2;0;0"
-            />
-          </circle>
-        </svg>
-      </div>
-    );
-
     if (hasReceivedValidResponse) {
       setChatMessages((prevMessages) => [
         ...prevMessages,
@@ -160,7 +111,48 @@ const ChatComponent = () => {
         ...prevMessages,
         {
           isAi: true,
-          message: timeoutMessageWithSVG,
+          message: (
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+            >
+              <circle cx="18" cy="12" r="0" fill="currentColor">
+                <animate
+                  attributeName="r"
+                  begin=".67"
+                  calcMode="spline"
+                  dur="1.5s"
+                  keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                  repeatCount="indefinite"
+                  values="0;2;0;0"
+                />
+              </circle>
+              <circle cx="12" cy="12" r="0" fill="currentColor">
+                <animate
+                  attributeName="r"
+                  begin=".33"
+                  calcMode="spline"
+                  dur="1.5s"
+                  keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                  repeatCount="indefinite"
+                  values="0;2;0;0"
+                />
+              </circle>
+              <circle cx="6" cy="12" r="0" fill="currentColor">
+                <animate
+                  attributeName="r"
+                  begin="0"
+                  calcMode="spline"
+                  dur="1.5s"
+                  keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8"
+                  repeatCount="indefinite"
+                  values="0;2;0;0"
+                />
+              </circle>
+            </svg>
+          ),
           uniqueId,
         },
       ]);
